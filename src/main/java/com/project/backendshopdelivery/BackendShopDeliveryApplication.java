e4eef2d94048b407a28bbeb83e085f3abe41ab1f
PhucPT09 <pphuc9122002@gmail.com>
2024-02-28 20:11:22 +0700

    init

---
+++ src/main/java/com/project/backendshopdelivery/BackendShopDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.project.backendshopdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShopDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShopDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/backendshopdelivery/BackendShopDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backendshopdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShopDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
